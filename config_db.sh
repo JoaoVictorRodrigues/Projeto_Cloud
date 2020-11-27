bde8b7f54261c2394421bb6b4d74f901df9bcb6b
joaovictorrodrigues <jvictor.rodrigues08@gmail.com>
2020-11-27 14:34:59 -0300

    Config db

--- config_db.sh
+++ config_db.sh
@@ -2,8 +2,8 @@ sudo apt install postgresql postgresql-contrib -y
 sudo -u postgres psql -c "CREATE USER cloud WITH PASSWORD 'cloud';"
 sudo -u postgres createdb -O cloud tasks
 sudo su - postgres
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf
-sudo echo host all all 192.168.0.0/20 trust >> /etc/postgresql/10/main/pg_hba.confcd /
-cd home/ubuntu
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/12/main/postgresql.conf
+sudo echo host all all 192.168.0.0/20 trust >> /etc/postgresql/12/main/pg_hba.confcd /
+exit
 sudo ufw allow 5432/tcp
 sudo systemctl restart postgresql
