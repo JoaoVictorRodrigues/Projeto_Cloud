3377095f6db66401247c5d71f5ad7b8be8b5d983
joaovictorrodrigues <jvictor.rodrigues08@gmail.com>
2020-11-27 15:53:29 -0300

    config db ajuste

--- config_db.sh
+++ config_db.sh
@@ -3,7 +3,7 @@ sudo -u postgres psql -c "CREATE USER cloud WITH PASSWORD 'cloud';"
 sudo -u postgres createdb -O cloud tasks
 sudo su - postgres
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/12/main/postgresql.conf
-sudo echo host all all 192.168.0.0/20 trust >> /etc/postgresql/12/main/pg_hba.confcd /
+sudo echo host all all 192.168.0.0/20 trust >> /etc/postgresql/12/main/pg_hba.conf
 exit
 sudo ufw allow 5432/tcp
 sudo systemctl restart postgresql
