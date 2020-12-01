b723fd7244e1c559d50f3a44bd60354fe8dcc109
joaovictorrodrigues <jvictor.rodrigues08@gmail.com>
2020-12-01 12:31:01 -0300

    Criacao de instancias e imagem funcionando

--- create_instance.py
+++ create_instance.py
@@ -21,7 +21,7 @@ waiterInicialize_NV = client_Inst_NV.get_waiter('instance_status_ok')
 waiterInicialize_Oh = client_Inst_Oh.get_waiter('instance_status_ok')
 
 
-def getIp(client, nome):
+def get_ip(client, nome):
     ip = client.describe_instances(
         Filters=[
             {
@@ -37,7 +37,21 @@ def getIp(client, nome):
     return(inst_ip)
 
 # Deleta uma instancia caso exista
-
+def get_instance_id(client, nome):
+    instance_id = client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [nome]
+            },
+            {
+                'Name': 'instance-state-name',
+                'Values': ['running']
+            }
+        ])
+    inst_id = instance_id['Reservations'][0]['Instances'][0]['InstanceId']
+    print(inst_id)
+    return(inst_id)
 
 def delete_instances(client, nome):
     instance_id = client.describe_instances(
@@ -95,8 +109,8 @@ def create_instance_Oh():
             ],
             UserData=userData_Oh
         )
-        waiterInicialize_Oh.wait(InstanceIds=[instance[0].id])
-        print("Fim do waiter")
+        client_Inst_Oh.get_waiter('instance_status_ok').wait(InstanceIds=[instance[0].id])
+        print("Ohio_DB criado e rodando")
 
     except ClientError as e:
         print(e)
@@ -116,7 +130,7 @@ def create_instance_NV():
     sudo reboot
     """.format(inst_ip)
     try:
-        instance_NV = client_Inst_NV.run_instances(
+        instance = resource_NV.create_instances(
             ImageId='ami-0885b1f6bd170450c',
             MinCount=1,
             MaxCount=1,
@@ -129,19 +143,28 @@ def create_instance_NV():
                     'Tags': [
                         {
                             'Key': 'Name',
-                            'Value': 'NV',
+                            'Value': 'NorthV_ORM',
                         },
                     ],
                 },
             ],
             UserData=userData_Nv
         )
+        client_Inst_NV.get_waiter('instance_status_ok').wait(InstanceIds=[instance[0].id])
+        print("NorthV_ORM criado e rodando")
     except ClientError as e:
         print(e)
 
+def create_AMI_ORM(instance_id,name):
+    ami = client_Inst_NV.create_image(InstanceId=instance_id, NoReboot=True, Name=name)
+    client_Inst_NV.get_waiter('image_available').wait(ImageIds=[ami["ImageId"]])
+    print("Imagem criada")
+    return ami['ImageId']
+    #delete_instances(client_Inst_NV, 'NorthV_ORM')
+
 
 delete_instances(client_Inst_Oh, 'Ohio_DB')
-delete_instances(client_Inst_NV, 'NV')
+delete_instances(client_Inst_NV, 'NorthV_ORM')
 
 delete_security_group(client_Inst_Oh, "SgOhio")
 delete_security_group(client_Inst_NV, "SgNorth")
@@ -149,8 +172,12 @@ delete_security_group(client_Inst_NV, "SgNorth")
 Ohio = create_security_group(client_Inst_Oh, "SgOhio")
 North = create_security_group(client_Inst_NV, "SgNorth")
 
+#North_instance_id = client_Inst_NV.describe_instance_status(InstanceIds=[id_instance_oregon[0].id])
 create_instance_Oh()
 
-inst_ip = getIp(client_Inst_Oh,'Ohio_DB')
-
+inst_ip = get_ip(client_Inst_Oh,'Ohio_DB')
 create_instance_NV()
+inst_id = get_instance_id(client_Inst_NV,'NorthV_ORM')
+print(inst_id)
+
+create_AMI_ORM(inst_id,'ORM')
