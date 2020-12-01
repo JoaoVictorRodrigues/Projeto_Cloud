1eedf7457e84d99491c2ea0ca94f30f32395339e
joaovictorrodrigues <jvictor.rodrigues08@gmail.com>
2020-12-01 10:38:05 -0300

    .

--- config_db.sh
+++ config_db.sh
@@ -3,7 +3,7 @@ sudo -u postgres psql -c "CREATE USER cloud WITH PASSWORD 'cloud';"
 sudo -u postgres createdb -O cloud tasks
 sudo su - postgres
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf
-echo host all all 192.168.0.0/20 trust >> /etc/postgresql/10/main/pg_hba.conf
+echo host all all 0.0.0.0/0 trust >> /etc/postgresql/10/main/pg_hba.conf
 cd /
 cd home/ubuntu
 sudo ufw allow 5432/tcp
